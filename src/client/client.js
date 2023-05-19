c0f78b92e569231838409a395f03dca27a4d5b5e
Phu Minh (Catto) <53084840+nguyenphuminh@users.noreply.github.com>
2023-05-19 11:13:43 +0700

    Fix

--- src/client/client.js
+++ src/client/client.js
@@ -20,7 +20,8 @@ const {
     LEVEL_PATH,
     LOG_FILE,
     FIRST_MINT_ADDR,
-    FIRST_MINT_AMOUNT
+    FIRST_MINT_AMOUNT,
+    SEQUENCER_MODE
 } = config;
 
 const bitrollKeyPair = ec.keyFromPrivate(PRIVKEY);
@@ -43,5 +44,67 @@ if (!fs.existsSync(LOG_FILE)) {
     fs.writeFileSync(LOG_FILE, counter.toString());
 } else {
     counter = parseInt(fs.readFileSync(LOG_FILE));
+}
+
+
+// Init client
+
+const Client = require('bitcoin-core');
+const client = new Client({
+    network: 'regtest',
+    port: 18443 
+});
+
+
+// Sync
+
+setTimeout(async function sync() {
+    try {
+
+        client.getBlockHash(counter)
+            .then(hash => client.getBlockByHash(hash))
+            .then(block => {
+                const transactionPromises = block.tx.map(txid => client.getRawTransaction(txid, true));
+
+                return Promise.all(transactionPromises);
+            })
+            .then(transactions => {
+                transactions.forEach(async transaction => {
+                    let recipient, sequencerAddress, batch;
+
+                    transaction.vout.forEach(vout => {
+                        if (vout.scriptPubKey.type !== 'nulldata') {
+                            recipient = vout.scriptPubKey.addresses[0];
+                        }
+
+                        if (vout.scriptPubKey.asm.startsWith('OP_RETURN')) {
+                            const parts = vout.scriptPubKey.asm.split(' ');
+                            const dataHex = parts[1];
+                            let data = Buffer.from(dataHex, 'hex').toString('utf8');
+
+                            sequencerAddress = data.slice(0, 64);
+                            data = data.slice(64);
+
+                            batch = decodeBatch(data);
+                        }
+                    });
+
+                    await transitState(batch, stateDB, sequencerAddress);
+                })
+            })
+    } catch(e) {
+        setTimeout(async () => await sync(), 120000);
+    }
+
+    counter++;
+
+    setTimeout(async () => await sync());
+}, 5000);
+
+
+// Sequencer
+
+if (SEQUENCER_MODE) {
+    
 }
 
